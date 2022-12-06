3dc2ba3f4d76bde5fe4cc8fcc6a7953e3a980361
isan-san <isan.9.9f@gmail.com>
2022-12-05 21:01:36 -0500

    first commit

---
+++ Listas.js
@@ -0,0 +1,10 @@
+birthdate = new Date ("march 20 1999");
+favBook = {
+    name : "Tokyo Vice",
+    description : "An American Reporter on the Police Beat in Japan",
+    yearOfRelese : 2010,
+    author : "Jake Adelstein"
+};
+newArray = ["Isan", 23, "isDeveloper", birthdate, favBook ];
+
+console.log(newArray);
